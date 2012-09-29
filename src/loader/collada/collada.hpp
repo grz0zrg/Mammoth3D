c4dc7939902f8d5ca2fb6ff5f609ef2a27530539
grz0zrg <grz.zrg@gmail.com>
2012-09-30 01:47:58 +0200

    Mesh parsing improvements

--- src/loader/collada/collada.hpp
+++ src/loader/collada/collada.hpp
@@ -1,8 +1,12 @@
 #ifndef COLLADA_HPP
 #define COLLADA_HPP
 
-	#include <iostream>
+	#include <iostream>  
+	#include <vector>
 	#include <map>
+	#include <cstring>
+	#include <cstdlib>
+	#include <cctype>
 
 	#include "tinyxml2/tinyxml2.h"
 	
@@ -16,14 +20,22 @@
 				};
 				
 				~Collada() {
+					cleanFloatArraysMap();
+
 					if(data) {
+						meshIt = data->mesh.begin();
+						while(meshIt != data->mesh.end()) {
+							if(meshIt->second) {
+								delete meshIt->second;
+							}
+						}
 						delete data;
 					}
 				};
 				
 			private:
 				void loadFile(const char *fileName) {
-					log("Loading: ", fileName);
+					log("Parsing: ", fileName);
 					int ret = doc.LoadFile(fileName);
 					if (ret != tinyxml2::XML_NO_ERROR) {
 						log("LoadFile error code: ", ret);
@@ -31,48 +43,169 @@
 					}
 						
 					tinyxml2::XMLElement* rootElement = doc.FirstChildElement("COLLADA");
+					const char *version = rootElement->Attribute("version");
+					if(version) {
+						log("Collada schema version: ", version);
+					}
 					
 					data = new Data;
 					
 					// traverse all libraries
 					int i = 0;
-					libType = libTypes[i];
+					e_libType libType = libTypes[i];
 					while(libType != NONE) {
 						tinyxml2::XMLElement* element = rootElement->FirstChildElement(libNames[i]);
 						if(element) {
 							log("Parsing element: ", libNames[i]);
-							element->Accept(this);
+							if(libType == GEOMETRIES) {
+								parseGeometries(element->FirstChildElement());
+							}
 						}
 						libType = libTypes[++i];
 					}
+					
+					// important step, this will resolve collada informations into basic mesh data
+					processData();
 				}
 				
-				virtual bool VisitEnter(const tinyxml2::XMLElement& element, 
-										const tinyxml2::XMLAttribute* firstAttribute) {
-						std::string elementName(element.Name());
-						if(&element) {
-						//	log(element.Name());
+				void parseGeometries(const tinyxml2::XMLElement* element) {
+					const tinyxml2::XMLElement* child = element;
+					while(child) {
+						element = child;
+						child = child->NextSiblingElement();
+
+						const char *elementName = element->Name();
+						const char *meshId = element->Attribute("id");
+						const char *meshName = element->Attribute("name");
+						if(strcmp(elementName, "geometry") != 0 || 
+											element->NoChildren() ||
+											!meshId) {
+							continue;
 						}
 						
-						if(firstAttribute) {
-							
-						}
+						// parse <geometry ...> childs
+						const tinyxml2::XMLElement* geomChild = 
+												element->FirstChildElement();
+						while(geomChild) {
+							const char *childName = geomChild->Name();
 
-						switch(libType) {
-							case GEOMETRIES:
-								//if (elementName == "geometry")
-								break;
+							// <mesh> content
+							if(!strcmp(childName, "mesh")) {
+								std::string idStr(meshId);
+								data->mesh[idStr] = new Mesh;
+								if(meshName) {
+									data->mesh[idStr]->name = meshName;
+								}
+								
+								parseMeshData(idStr, geomChild->FirstChildElement());
+							} else if (!strcmp(childName, "extra")) { // <extra>
+							}
 
-							default: break;
+							geomChild = geomChild->NextSiblingElement();
 						}
-					/*log(element.Name());
-					if(firstAttribute)
-					log(firstAttribute->Value());*/
-					return true;
+					}
 				}
 				
-				virtual bool VisitExit(const tinyxml2::XMLElement &element) {
-					return true;
+				void parseMeshData(const std::string &str, const tinyxml2::XMLElement* element) {
+					while(element) {
+						const tinyxml2::XMLElement* sourceChild = 
+												element->FirstChildElement();
+						// <source>
+						if (!strcmp(element->Name(), "source")) {
+							const char* id = element->Attribute("id");
+							while(sourceChild) {
+								//<float_array ...>
+								if (!strcmp(sourceChild->Name(), "float_array")) {
+									// parse&store float array
+									const char *text = sourceChild->GetText();
+									if(id && text) {
+										std::vector<std::string> floatData = 
+																splitText(text);
+										
+										unsigned int count = sourceChild->IntAttribute("count");
+										unsigned int realSize = floatData.size();
+										if(count) {
+											if(count == realSize) {
+												floatArrays[id] = new float[realSize];
+												for(unsigned int i=0; i<realSize; i++) {
+													floatArrays[id][i] = (float)atof(floatData[i].c_str());
+												}
+											} else {
+												log(id, " float array size does not match count attribute");
+											}
+										}
+									}
+								}
+								sourceChild = sourceChild->NextSiblingElement();
+							}
+						// <vertices>
+						} else if (!strcmp(element->Name(), "vertices")) {
+							while(sourceChild) {	
+								// <input ...>
+								if (!strcmp(sourceChild->Name(), "input")) {
+									const char* semantic = element->Attribute("semantic");
+									const char* source = element->Attribute("source");
+									if(semantic && source) {
+										semanticLink[std::string(semantic)] = std::string(source);
+									}
+								}
+								
+								sourceChild = sourceChild->NextSiblingElement();
+							}
+						// <polylist> <triangles> etc...
+						} else if (!strcmp(element->Name(), "polylist") ||
+									!strcmp(element->Name(), "triangles") ||
+									!strcmp(element->Name(), "lines") ||
+									!strcmp(element->Name(), "linestrips") ||
+									!strcmp(element->Name(), "polygons") ||
+									!strcmp(element->Name(), "trifans") ||
+									!strcmp(element->Name(), "tristrips")) {
+							unsigned int count = element->IntAttribute("count");
+							while(sourceChild) {
+								// <input ...>
+								if (!strcmp(sourceChild->Name(), "input")) {
+									const char* semantic = element->Attribute("semantic");
+									const char* source = element->Attribute("source");
+									if(semantic && source) {
+										semanticLink[std::string(semantic)] = std::string(source);
+									}
+								// <vcount>
+								} else if (!strcmp(sourceChild->Name(), "vcount")) {
+									const char *text = sourceChild->GetText();
+									if (text) {
+										std::vector<std::string> floatData = 
+																splitText(text);										
+									}
+								// <p>
+								} else if (!strcmp(sourceChild->Name(), "p")) {
+									const char *text = sourceChild->GetText();
+									if (text) {
+										std::vector<std::string> floatData = 
+																splitText(text);										
+									}
+								}
+
+								sourceChild = sourceChild->NextSiblingElement();
+							}
+						}
+						
+						element = element->NextSiblingElement();
+					}
+				}
+				
+				// 
+				void processData() {
+					
+				}
+				
+				void cleanFloatArraysMap() {
+					floatArraysIt = floatArrays.begin();
+					while(floatArraysIt != floatArrays.end()) {
+						if(floatArraysIt->second) {
+							delete[] floatArraysIt->second;
+						}
+					}
+					floatArrays.clear();
 				}
 				
 				template <typename T>
@@ -83,9 +216,28 @@
 				void log(const char *str) {
 					std::cout << "[COLLADA] " << str << std::endl;
 				}
+				
+				// split text, delimited by whitespaces
+				std::vector<std::string> splitText(const char *str) {
+					std::string line;
+					std::vector<std::string> tokens;
+
+					while(*str) {
+						while(isspace(*str) && *str) str++;
+
+						line += *str++;
+						
+						if(isspace(*str) || !*str) {
+							tokens.push_back(line);
+							line.clear();
+						}
+					}
+
+					return tokens;
+				}
 
 				typedef struct {
-					std::string id;
+					const char *name;
 
 					//std::map<std::string, std::string> positions;
 				} Mesh;
@@ -93,16 +245,25 @@
 				typedef struct {
 					std::map<std::string, Mesh*> mesh;
 				} Data;
+				
+				// used to store float arrays with their source link
+				std::map<std::string, float*> floatArrays;
+				
+				// used to store semantic-source link
+				std::map<std::string, std::string> semanticLink;
+				
+				// iterators
+				std::map<std::string, float*>::iterator floatArraysIt;
+				std::map<std::string, Mesh*>::iterator meshIt;
 
-				// used to parse each 'libraries'
 				enum e_libType{ ANIMATIONS, ASSET, CONTROLLERS, CAMERAS,
 								EFFECTS, GEOMETRIES, IMAGES, LIGHTS,
 								MATERIALS, SCENE, VISUAL_SCENES, NONE};
 								
 				tinyxml2::XMLDocument doc;
 				Data *data;
-				e_libType libType;
 				
+				// define collada libraries to parse
 				const static char *libNames[];
 				const static e_libType libTypes[];
 		};
