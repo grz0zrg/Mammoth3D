9bae3cb1975730df08648f0cec1b269e73225a2b
grz <grz@grz-deb>
2014-06-09 21:07:59 +0200

    Added Linux project configurations for CodeLite

--- README.md
+++ README.md
@@ -11,25 +11,25 @@ Disclaimer: The framework is kinda messy, this is my first try at a 3D engine ev
 
 ### Usage ###
 
-All projects should compile easily on Windows using the [codelite](http://www.codelite.org/) .workspace.
+There is a sample project which is just a test area in the "project" folder, this can be used as a starting point.
 
-There is a sample project demonstrating some features in the "project" folder, this can be used as a starting point.
+The sample project should compile easily on Windows/Linux using the [codelite](http://www.codelite.org/) .workspace, there is configurations for Windows and Linux.
 
-The "lib" folder contain all the libraries the engine use, some are mingw32 x86 pre-compiled
+The "lib" folder contain all the libraries the engine use, some are mingw32 x86 pre-compiled so it compile easily under Windows
 
 ### Tools ###
 
 The engine use a binary format to load scenes/meshes, converters are available in the "tools" folder
 
-assimp2bin load any meshes files that the Assimp library support [Assimp](http://assimp.sourceforge.net/)
+assimp2bin load any meshes files that the [Assimp](http://assimp.sourceforge.net/) library support
 
 dae2bin is deprecated, export Collada (.dae) only and only meshes vertices (UVs/normals exports are buggy)
 
 ### Others platforms ###
 
-Just look at the .project settings in the project folder, the framework require: [PortAudio](http://www.portaudio.com/) [libogg/libvorbis](http://xiph.org/downloads) [glfw](http://www.glfw.org/download.html)
+Just look at the .project settings in the project folder, the framework require: [PortAudio](http://www.portaudio.com/) [libogg/libvorbis](http://xiph.org/downloads) [glfw3](http://www.glfw.org/download.html)
 
-Additionally some others libraries ([lodepng](http://lodev.org/lodepng/), [GLEW](http://glew.sourceforge.net/), [tinyxml2](http://www.grinninglizard.com/tinyxml2/)) are used and can be found in the "lib" folder, they can be compiled with your app 
+Additionally some others libraries ([lodepng](http://lodev.org/lodepng/), [GLEW](http://glew.sourceforge.net/), [tinyxml2](http://www.grinninglizard.com/tinyxml2/)) are provided and used, they can be found in the "lib" folder, they can be compiled with the app
 
 The assimp2bin tool require the [Assimp](http://assimp.sourceforge.net/) library
 
--- tools/assimp2bin/main.cpp
+++ tools/assimp2bin/main.cpp
@@ -2,28 +2,29 @@
  * This program load any formats supported by the assimp library
  * and export it to a binary format usable directly within the Mammoth3D engine.
  * 
- * Usage: assimb2bin input_file
+ * Usage: assimb2bin input_file output_file
  * 
  * Note: only a small subset of data is exported, indices/vertices/normals/uvs/colors
  *       and the geometry is always assumed to be made of triangles (assimp triangulate anyway)
  * */
 
+
 #include "../../include/assimp/Importer.hpp"
 #include "../../include/assimp/scene.h"
 #include "../../include/assimp/postprocess.h"
-
+	
 #include <cstdio>
 #include <string>
 #include <iostream>
 #include <fstream>
 
-std::string doExport(const aiScene* scene);
+std::string doExport(const aiScene* scene, const std::string& output_filename);
 
 int main(int argc, char **argv)
 {
 	std::cout << "assimp2bin.exe - Mammoth3D tool\n\n";
 
-	if (argc != 2) {
+	if (argc != 3) {
 		std::cout << "Invalid usage.\n\n";
 		return 0;
 	}
@@ -39,129 +40,158 @@ int main(int argc, char **argv)
 		return 0;
 	}
 	
-	std::cout << doExport(scene);
+	std::cout << doExport(scene, std::string(argv[2]));
 
 	return 1;
 }
 
-std::string doExport(const aiScene* scene) {
+std::string doExport(const aiScene* scene, const std::string& output_filename) {
 	if (scene->HasMeshes()) {
-		for (unsigned int i = 0; i < scene->mNumMeshes; i++) {
-			const aiMesh *aimesh = scene->mMeshes[i];
-			
-			std::string filename(aimesh->mName.C_Str());
-			if (filename.empty()) {
-				filename = "mesh.mm";
-			} else {
-				filename += ".mm";
-			}
-			
-			std::ofstream file(filename.c_str(), std::ios::out | std::ios::binary);
-			if (!file.good() || !file.is_open()) {
-				return std::string("Failed to export Mesh: " + std::string(filename));
-			}
-			
-			// required to be compatible with the old custom collada loader
-			char indicesType = 0;
-			file.write((char*)&indicesType, sizeof(indicesType));
-
-			unsigned int indicesCount = 0;
-			unsigned int verticesCount = 0;
-			unsigned int normalsCount = 0;
-			unsigned int texcoordsCount = 0;
-			unsigned int vColorsCount = 0;
-			
-			unsigned int numNormals = 0;
-			unsigned int numUvs = 0;
-			unsigned int numVColors = 0;
-			
-			if (aimesh->HasFaces()) {
-				indicesCount = aimesh->mNumFaces * 3; // only triangles
-			}
-
-			if (aimesh->HasPositions()) {
-				verticesCount = aimesh->mNumVertices * 3;
-			}
-			
-			if (aimesh->HasNormals() && aimesh->mNormals != NULL) {
-				numNormals = aimesh->mNumVertices;
-				normalsCount = verticesCount;
-			}
-			
-			if (aimesh->HasTextureCoords(0) && aimesh->mTextureCoords[0] != NULL) {
-				numUvs = aimesh->mNumVertices;
-				texcoordsCount = numUvs * 2;
-			}
-			
-			if (aimesh->HasVertexColors(0) && aimesh->mColors[0] != NULL) {
-				numVColors = aimesh->mNumVertices;
-				vColorsCount = numVColors * 4;
-			}
-
-			file.write((char*)&indicesCount, sizeof(indicesCount));
-			file.write((char*)&verticesCount, sizeof(verticesCount));
-			file.write((char*)&normalsCount, sizeof(normalsCount));
-			file.write((char*)&texcoordsCount, sizeof(texcoordsCount));
-
-			for (unsigned int i = 0; i < aimesh->mNumFaces; i++) {
-				const aiFace *face = &aimesh->mFaces[i];
-				
-				for (unsigned int j = 0; j < 3; j++) {
-					file.write((char*)&face->mIndices[j], sizeof(unsigned int));
-				}
-			}
-
-			for (unsigned int i = 0; i < aimesh->mNumVertices; i++) {
-				const aiVector3D *vertice = &aimesh->mVertices[i];
-				
-				file.write((char*)&vertice->x, sizeof(float));
-				file.write((char*)&vertice->y, sizeof(float));
-				file.write((char*)&vertice->z, sizeof(float));
-			}
-
-			for (unsigned int i = 0; i < numNormals; i++) {
-				const aiVector3D *normal = &aimesh->mNormals[i];
-				
-				file.write((char*)&normal->x, sizeof(float));
-				file.write((char*)&normal->y, sizeof(float));
-				file.write((char*)&normal->z, sizeof(float));
-			}
-
-			// this is done here so the format remain compatible with the old dae2bin tool
-			file.write((char*)&vColorsCount, sizeof(vColorsCount));
-
-			for (unsigned int i = 0; i < numVColors; i++) {
-				const aiColor4D *color = &aimesh->mColors[0][i];
-				
-				file.write((char*)&color->r, sizeof(float));
-				file.write((char*)&color->g, sizeof(float));
-				file.write((char*)&color->b, sizeof(float));
-				file.write((char*)&color->a, sizeof(float));
-			}
-
-			for (unsigned int i = 0; i < numUvs; i++) {
-				const aiVector3D *uv = &aimesh->mTextureCoords[0][i];
-				
-				file.write((char*)&uv->x, sizeof(float));
-				file.write((char*)&uv->y, sizeof(float));
-			}
-
-			if (scene->HasMaterials()) {
-				const aiMaterial *material = scene->mMaterials[aimesh->mMaterialIndex];
-				
-				aiString diffuse_filename;
-				unsigned int diffuse_filename_length = 0;
-				if (material->Get(AI_MATKEY_TEXTURE(aiTextureType_DIFFUSE, 0), diffuse_filename) == AI_SUCCESS) {
-					diffuse_filename_length = diffuse_filename.length+1;
-					file.write((char*)&diffuse_filename_length, sizeof(unsigned int));
-					file.write((char*)diffuse_filename.C_Str(), diffuse_filename_length);
-				} else {
-					file.write((char*)&diffuse_filename_length, sizeof(unsigned int));
-				}
-			}
-			
-			file.close();
+		std::string filename(output_filename);
+		if (filename.empty()) {
+			filename = "mesh";
 		}
+
+		filename += ".mm";
+			
+		std::ofstream file(filename.c_str(), std::ios::out | std::ios::binary);
+		if (!file.good() || !file.is_open()) {
+			return std::string("Failed to export Mesh: " + filename);
+		}
+		
+		const aiNode *root = scene->mRootNode;
+		
+		unsigned int numNodes = root->mNumChildren;
+		file.write((char*)&numNodes, sizeof(numNodes));
+		
+		for (unsigned int i = 0; i < root->mNumChildren; i++) {
+			const aiNode *child = root->mChildren[i];
+			
+			// ignore non meshs stuff for now
+			unsigned int numMeshes = child->mNumMeshes;
+			/*if (numMeshes == 0) {
+				continue;
+			}*/
+			
+			file.write((char*)&numMeshes, sizeof(numMeshes));
+			
+			aiMatrix4x4 tmatrix = root->mTransformation*child->mTransformation;
+			file.write((char*)&tmatrix.a1, sizeof(float));
+			file.write((char*)&tmatrix.a2, sizeof(float));
+			file.write((char*)&tmatrix.a3, sizeof(float));
+			file.write((char*)&tmatrix.a4, sizeof(float));
+			file.write((char*)&tmatrix.b1, sizeof(float));
+			file.write((char*)&tmatrix.b2, sizeof(float));
+			file.write((char*)&tmatrix.b3, sizeof(float));
+			file.write((char*)&tmatrix.b4, sizeof(float));
+			file.write((char*)&tmatrix.c1, sizeof(float));
+			file.write((char*)&tmatrix.c2, sizeof(float));
+			file.write((char*)&tmatrix.c3, sizeof(float));
+			file.write((char*)&tmatrix.c4, sizeof(float));
+			file.write((char*)&tmatrix.d1, sizeof(float));
+			file.write((char*)&tmatrix.d2, sizeof(float));
+			file.write((char*)&tmatrix.d3, sizeof(float));
+			file.write((char*)&tmatrix.d4, sizeof(float));
+			
+			for (unsigned int i = 0; i < child->mNumMeshes; i++) {
+				const aiMesh *aimesh = scene->mMeshes[child->mMeshes[i]];
+
+				unsigned int indicesCount = 0;
+				unsigned int verticesCount = 0;
+				unsigned int normalsCount = 0;
+				unsigned int texcoordsCount = 0;
+				unsigned int vColorsCount = 0;
+			
+				unsigned int numNormals = 0;
+				unsigned int numUvs = 0;
+				unsigned int numVColors = 0;
+				
+				if (aimesh->HasFaces()) {
+					indicesCount = aimesh->mNumFaces * 3; // only triangles
+				}
+
+				if (aimesh->HasPositions()) {
+					verticesCount = aimesh->mNumVertices * 3;
+				}
+				
+				if (aimesh->HasNormals() && aimesh->mNormals != NULL) {
+					numNormals = aimesh->mNumVertices;
+					normalsCount = verticesCount;
+				}
+				
+				if (aimesh->HasTextureCoords(0) && aimesh->mTextureCoords[0] != NULL) {
+					numUvs = aimesh->mNumVertices;
+					texcoordsCount = numUvs * 2;
+				}
+				
+				if (aimesh->HasVertexColors(0) && aimesh->mColors[0] != NULL) {
+					numVColors = aimesh->mNumVertices;
+					vColorsCount = numVColors * 4;
+				}
+
+				file.write((char*)&indicesCount, sizeof(indicesCount));
+				file.write((char*)&verticesCount, sizeof(verticesCount));
+				file.write((char*)&normalsCount, sizeof(normalsCount));
+				file.write((char*)&texcoordsCount, sizeof(texcoordsCount));
+				file.write((char*)&vColorsCount, sizeof(vColorsCount));
+
+				for (unsigned int i = 0; i < aimesh->mNumFaces; i++) {
+					const aiFace *face = &aimesh->mFaces[i];
+					
+					for (unsigned int j = 0; j < 3; j++) {
+						file.write((char*)&face->mIndices[j], sizeof(unsigned int));
+					}
+				}
+
+				for (unsigned int i = 0; i < aimesh->mNumVertices; i++) {
+					const aiVector3D *vertice = &aimesh->mVertices[i];
+					
+					file.write((char*)&vertice->x, sizeof(float));
+					file.write((char*)&vertice->y, sizeof(float));
+					file.write((char*)&vertice->z, sizeof(float));
+				}
+
+				for (unsigned int i = 0; i < numNormals; i++) {
+					const aiVector3D *normal = &aimesh->mNormals[i];
+					
+					file.write((char*)&normal->x, sizeof(float));
+					file.write((char*)&normal->y, sizeof(float));
+					file.write((char*)&normal->z, sizeof(float));
+				}
+
+				for (unsigned int i = 0; i < numVColors; i++) {
+					const aiColor4D *color = &aimesh->mColors[0][i];
+					
+					file.write((char*)&color->r, sizeof(float));
+					file.write((char*)&color->g, sizeof(float));
+					file.write((char*)&color->b, sizeof(float));
+					file.write((char*)&color->a, sizeof(float));
+				}
+
+				for (unsigned int i = 0; i < numUvs; i++) {
+					const aiVector3D *uv = &aimesh->mTextureCoords[0][i];
+					
+					file.write((char*)&uv->x, sizeof(float));
+					file.write((char*)&uv->y, sizeof(float));
+				}
+
+				if (scene->HasMaterials()) {
+					const aiMaterial *material = scene->mMaterials[aimesh->mMaterialIndex];
+					
+					aiString diffuse_filename;
+					unsigned int diffuse_filename_length = 0;
+					if (material->Get(AI_MATKEY_TEXTURE(aiTextureType_DIFFUSE, 0), diffuse_filename) == AI_SUCCESS) {
+						diffuse_filename_length = diffuse_filename.length+1;
+						file.write((char*)&diffuse_filename_length, sizeof(unsigned int));
+						file.write((char*)diffuse_filename.C_Str(), diffuse_filename_length);
+					} else {
+						file.write((char*)&diffuse_filename_length, sizeof(unsigned int));
+					}
+				}
+			}
+		}
+		
+		file.close();
 	}
 	
 	return std::string("success");
